52ea03bbe3c11ea3ed80795de460786fafccf8d1
Oleksandra Antonova <werba2010@gmail.com>
2023-08-15 22:23:01 -0400

    adding structure and responsive menu

---
+++ js/script.js
@@ -0,0 +1,61 @@
+let mobileMenu = document.querySelector(".mobileMenu");
+let menuItems = document.querySelectorAll(".itemsMenu");
+let menuLinks = document.querySelectorAll(".itemsMenu a");
+let icons = document.querySelectorAll(".itemsMenu i.fa-solid");
+
+// Mobile menu show/hide
+mobileMenu.addEventListener("click", function () {
+  menuItems.forEach(function(item) {
+    item.classList.toggle("active");
+  });
+});
+
+// Close mobile menu on item click
+menuItems.forEach(function(item) {
+  item.addEventListener("click", function () {
+    menuItems.forEach(function(item) {
+      item.classList.remove("active");
+    });
+  });
+});
+
+// Active menu item on click
+menuLinks.forEach(function(link) {
+  link.addEventListener("click", function(event) {
+    menuLinks.forEach(function(link) {
+      link.closest(".itemsMenu").classList.remove("hoverMenu");
+    });
+    this.closest(".itemsMenu").classList.add("hoverMenu");
+  });
+});
+
+// Menu item hover effect
+menuItems.forEach(function(menuItem) {
+    menuItem.addEventListener("mouseenter", function () {
+    let href = this.querySelector("a");
+        if (href) {
+            href.style.color = "rgb(255, 106, 0)";
+        }
+
+    let icon = this.querySelector("i.fa-solid");
+    if (icon) {
+      icon.style.transform = "scale(1.3)";
+      icon.style.transition = "transform 0.3s ease";
+      icon.style.color = "rgb(255, 106, 0)";
+    }
+  });
+
+    menuItem.addEventListener("mouseleave", function () {
+    let href = this.querySelector("a");
+      if (href) {
+            href.style.color = "#030303";
+        }
+
+    let icon = this.querySelector("i.fa-solid");
+    if (icon) {
+      icon.style.transform = "rotate(90deg)";
+        icon.style.transition = "transform 0.3s ease";
+        icon.style.color = "#030303";
+    }
+  });
+});

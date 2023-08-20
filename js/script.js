327af725a31e3bfc560566cb00f743bb8362e876
Oleksandra Antonova <werba2010@gmail.com>
2023-08-19 23:37:58 -0400

    adding info & images for home, about sections

--- js/script.js
+++ js/script.js
@@ -98,11 +98,13 @@ window.addEventListener("scroll", function() {
     }
   });
 });
+
+
 // Adding Pulse Effect
 
 function addPulseEffect() {
-  const originalBoxShadow = "0 0 30px rgb(255, 106, 0)";
-  const expandedBoxShadow = "0 0 70px rgb(255, 106, 0)";
+  const originalBoxShadow = "0 0 10px #457032";
+  const expandedBoxShadow = "0 0 30px #457032";
 
   imgElement.style.boxShadow = originalBoxShadow;
 
@@ -114,7 +116,7 @@ function addPulseEffect() {
       imgElement.style.transition = "box-shadow 0.5s ease-in-out";
       imgElement.style.boxShadow = originalBoxShadow;
 
-      if (count < 2) {
+      if (count < 1) {
         setTimeout(() => {
           pulse(count + 1);
         }, 1500); 
@@ -126,6 +128,67 @@ function addPulseEffect() {
 
 addPulseEffect();
 
+// new cursor
 
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.innerWidth >= 991) {
+    const cursor = document.getElementById('cursor');
+    const cursorOutline = document.getElementById('cursor-outline');
+
+    function updateCursorPosition(event) {
+      cursor.style.left = event.clientX + 'px';
+      cursor.style.top = event.clientY + window.scrollY + 'px';
+
+      setTimeout(() => {
+        cursorOutline.style.left = event.clientX + 'px';
+        cursorOutline.style.top = event.clientY + window.scrollY + 'px';
+      }, 100);
+    }
+
+    document.addEventListener('mousemove', updateCursorPosition);
+
+    window.addEventListener('scroll', function (event) {
+      const lastX = parseFloat(cursor.style.left);
+      const lastY = parseFloat(cursor.style.top) - window.scrollY;
+
+      cursor.style.left = lastX + 'px';
+      cursor.style.top = lastY + window.scrollY + 'px';
+
+      setTimeout(() => {
+        cursorOutline.style.left = lastX + 'px';
+        cursorOutline.style.top = lastY + window.scrollY + 'px';
+      }, 100);
+    });
+
+    document.getElementById('btn').addEventListener('mouseover', function () {
+      cursorOutline.style.height = '25px';
+      cursorOutline.style.width = '25px';
+    });
+
+    document.getElementById('btn').addEventListener('mouseleave', function () {
+      cursorOutline.style.height = '15px';
+      cursorOutline.style.width = '15px';
+    });
+    
+  }
+});
+
+
+// icons opacity
+
+document.addEventListener('DOMContentLoaded', function () {
+  const iconsContainer = document.getElementById('icons');
+  const icons = document.querySelectorAll('.icons-image');
+
+  iconsContainer.style.opacity = 1;
+
+  icons.forEach(function (icon, index) {
+    setTimeout(function () {
+      icon.style.opacity = 1;
+      icon.style.transform = 'translateY(0)';
+    }, index * 500); 
+  });
+  
+});
 
 
